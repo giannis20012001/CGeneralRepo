e2a24ddbbf6313035c2d85fb414c233e4ce022ed
John Tsantilis <i.tsantilis@yahoo.com>
2015-12-18 16:47:02 +0200

    Bob is painting

---
+++ first/ShapesWithAsterisks.c
@@ -0,0 +1,20 @@
+//
+// Created by John Tsantilis (A.K.A lumi) on 18/12/2015.
+//
+
+#include <stdio.h>
+
+int main () {
+    printf("%s \t %s \t %s \t %s \n", "*********", "  ***  ", "  *  ", "    *    ");
+    printf("%s \t %s \t %s \t %s \n", "*       *", " *   * ", " *** ", "   * *   ");
+    printf("%s \t %s \t %s \t %s \n", "*       *", "*     *", "*****", "  *   *  ");
+    printf("%s \t %s \t %s \t %s \n", "*       *", "*     *", "  *  ", " *     * ");
+    printf("%s \t %s \t %s \t %s \n", "*       *", "*     *", "  *  ", "*       *");
+    printf("%s \t %s \t %s \t %s \n", "*       *", "*     *", "  *  ", " *     * ");
+    printf("%s \t %s \t %s \t %s \n", "*       *", "*     *", "  *  ", "  *   *  ");
+    printf("%s \t %s \t %s \t %s \n", "*       *", " *   * ", "  *  ", "   * *   ");
+    printf("%s \t %s \t %s \t %s \n", "*********", "  ***  ", "  *  ", "    *    ");
+
+    return 0;
+
+}
