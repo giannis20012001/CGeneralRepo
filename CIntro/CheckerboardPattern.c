e983d5936dddbc149126eed42db38ac43fe16c41
John Tsantilis <i.tsantilis@yahoo.com>
2015-12-18 17:47:12 +0200

    Bob is painting

--- CIntro/CheckerboardPattern.c
+++ CIntro/CheckerboardPattern.c
@@ -2,3 +2,23 @@
 // Created by John Tsantilis (A.K.A lumi) on 18/12/2015.
 //
 
+#include <stdio.h>
+
+int main () {
+    printf("%s \n", "********");
+    printf("%s \n", " ********");
+    printf("%s \n", "********");
+    printf("%s \n", " ********");
+    printf("%s \n", "********");
+    printf("%s \n", " ********");
+    printf("%s \n", "********");
+    printf("%s \n", " ********");
+
+    puts("");
+
+    printf("%s \n %s \n %s \n %s \n %s \n %s \n %s \n %s \n", "********", " ********",
+           "********", " ********", "********", " ********", "********", " ********");
+
+    return 0;
+
+}
