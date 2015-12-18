13cd8e8dcb42623b2020650690fb3c447a470c9b
John Tsantilis <i.tsantilis@yahoo.com>
2015-12-18 17:24:13 +0200

    Bob is painting

--- CFirst/Initials.c
+++ CFirst/Initials.c
@@ -2,3 +2,23 @@
 // Created by John Tsantilis (A.K.A lumi) on 18/12/2015.
 //
 
+#include <stdio.h>
+
+int main () {
+    printf("%s \n", "  JJ     ");
+    printf("%s \n", " J       ");
+    printf("%s \n", "J        ");
+    printf("%s \n", " J       ");
+    printf("%s \n", "  JJJJJJJ");
+    puts("");
+
+    printf("%s \n", "        T");
+    printf("%s \n", "        T");
+    printf("%s \n", "TTTTTTTTT");
+    printf("%s \n", "        T");
+    printf("%s \n", "        T");
+    printf("%s \n", "        T");
+
+    return 0;
+
+}
