0a8b29e002726d3a6e474de2ab193d0f99c7148c
John Tsantilis <giannis20012001@yahoo.com>
2016-01-15 17:39:13 +0200

    Bob is painting

--- CStructuredProgrammingDev/InterestCalculator.c
+++ CStructuredProgrammingDev/InterestCalculator.c
@@ -2,3 +2,29 @@
 // Created by John Tsantilis (A.K.A lumi) on 15/1/2016.
 //
 
+#include <stdio.h>
+
+int main (void) {
+    float term = 0;
+    float rate = 0;
+    float principal = 0;
+    float interest = 0;
+
+    printf("%s", "Enter loan principal (-1 to end): ");
+    scanf("%f", &principal);
+    while (principal != -1) {
+        printf("%s", "Enter interest rate: ");
+        scanf("%f", &rate);
+        printf("%s", "Enter term of the loan in days: ");
+        scanf("%f", &term);
+
+        interest = principal * rate * (term/365);
+        printf("%s %0.2f\n\n", "The interest charge is $", interest);
+
+        printf("%s", "Enter loan principal (-1 to end): ");
+        scanf("%f", &principal);
+
+    }
+    return 0;
+
+}
