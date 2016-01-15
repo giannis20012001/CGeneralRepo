bca37069e983ed25cada179ce7a8f9880e78b4fc
John Tsantilis <giannis20012001@yahoo.com>
2016-01-15 16:17:18 +0200

    Bob is painting

--- CStructuredProgrammingDev/SalesCommissionCalculator.c
+++ CStructuredProgrammingDev/SalesCommissionCalculator.c
@@ -5,7 +5,21 @@
 #include <stdio.h>
 
 int main (void) {
+    int baseSalary = 200;
+    float sales = 0;
+    float totalSalary = 0;
+    float commission = 0.09;
 
+    printf("%s", "Enter sales in dollars (-1 to end): ");
+    scanf("%f", &sales);
+    while (sales != -1) {
+        totalSalary = baseSalary + (sales * commission);
+        printf("%s %0.2f\n\n", "Salary is: $", totalSalary);
+
+        printf("%s", "Enter sales in dollars (-1 to end): ");
+        scanf("%f", &sales);
+
+    }
     return 0;
 
 }
