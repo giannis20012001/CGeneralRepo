172e6bf9582dc1025c9338d083f805cf758febf0
John Tsantilis <i.tsantilis@yahoo.com>
2016-01-21 19:00:41 +0200

    Bob is painting

---
+++ CStructuredProgrammingDev/CheckerboardPatternOfAsterisks.c
@@ -0,0 +1,19 @@
+//
+// Created by John Tsantilis (A.K.A lumi) on 21/1/2016.
+//
+
+#include <stdio.h>
+
+int main (void) {
+    int externalCounter = 1;
+    int internalCounter = 1;
+
+    while (externalCounter <= 8) {
+        printf("%s", "* ");
+
+    }
+    puts("Finished adding!!!");
+
+    return 0;
+
+}
