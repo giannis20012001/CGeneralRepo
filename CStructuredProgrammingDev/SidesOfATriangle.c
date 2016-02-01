40a5e26a2a96eb27ecb704fb13b4ebaa635db72f
John Tsantilis <i.tsantilis@yahoo.com>
2016-02-01 15:42:37 +0200

    Bob is painting

--- CStructuredProgrammingDev/SidesOfATriangle.c
+++ CStructuredProgrammingDev/SidesOfATriangle.c
@@ -2,3 +2,44 @@
 // Created by John Tsantilis (A.K.A lumi) on 1/2/2016.
 //
 
+#include <stdio.h>
+
+int main (void) {
+    int firstSide;
+    int secondSide;
+    int thirdSide;
+
+    printf("%s", "Enter first side(0 to exit): ");
+    scanf("%d", &firstSide);
+    while (firstSide != 0) {
+        printf("%s", "Enter second sie: ");
+        scanf("%d", &secondSide);
+        printf("%s", "Enter third sie: ");
+        scanf("%d", &thirdSide);
+        if (firstSide < 0) {
+            puts("You have provided a negative side!!!");
+            puts("Please provide a new number!!!");
+
+        }
+        else if (secondSide < 0) {
+            puts("You have provided a negative side!!!");
+            puts("Please provide a new number!!!");
+
+        }
+        else if (thirdSide < 0) {
+            puts("You have provided a negative side!!!");
+            puts("Please provide a new number!!!");
+
+        }
+        else {
+            puts("The given sides can represent a right triangle");
+
+        }
+        printf("\n%s", "Enter first side(0 to exit): ");
+        scanf("%d", &firstSide);
+
+    }
+
+    return 0;
+
+}
